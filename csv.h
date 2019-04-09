ff568cad1ecedffcf334f1c11adf82dcf14f689e
Aaron Chang <aaronchang153@gmail.com>
2019-04-09 16:15:26 -0700

    Added csv.h with read_csv function

---
+++ csv.h
@@ -0,0 +1,14 @@
+#ifndef CSV_H
+#define CSV_H
+
+#include <string>
+#include <vector>
+
+typedef struct{
+    int studentID;
+    float grade;
+} student_data;
+
+std::vector<student_data> read_csv(std::string fname);
+
+#endif // CSV_H
