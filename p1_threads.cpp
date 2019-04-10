2e45faf1c95781dd4c2823a88248bd64ab34ff87
Aaron Chang <aaronchang153@gmail.com>
2019-04-09 21:09:35 -0700

    Now sorts a vector of ID/grade instead of int. Appears to be working fine.

--- p1_threads.cpp
+++ p1_threads.cpp
@@ -3,33 +3,32 @@
 #define AVG(a, b) (((a) + (b)) / 2)
 
 typedef struct{
-	int *data;
+	DATA_VECTOR *data;
 	int start;
 	int end;
 } merge_sort_thread_args;
 
-void merge(int data[], int low, int mid, int high){
+void merge(DATA_VECTOR &data, int low, int mid, int high){
 	// split data into left and right sections
 	// NOTE: mid is the index of the last element of the left array
 	int size_left  = mid - low + 1;
 	int size_right = high - mid;
 
-	int *left = new int[size_left];
-	int *right = new int[size_right];
+	DATA_VECTOR left, right;
 
 	int i, j;
 	for(i = 0; i < size_left; i++)
-		left[i] = data[low + i];
+		left.push_back(data[low + i]);
 	// at this point, low+i is the index after the last element in left
 	// i.e. the first element of right
 	for(j = 0; j < size_right; j++)
-		right[j] = data[mid + j + 1];
+		right.push_back(data[mid + j + 1]);
 
 	// fill data with the minimum elements from left and right in order
 	i = j = 0; // current position in left and right respectively
 	int n = low; // current position in original data array
 	while(i < size_left && j < size_right){
-		if(left[i] < right[j])
+		if(left[i].grade < right[j].grade)
 			data[n++] = left[i++];
 		else
 			data[n++] = right[j++];
@@ -40,13 +39,10 @@ void merge(int data[], int low, int mid, int high){
 	// include the rest of the right array
 	while(j < size_right)
 		data[n++] = right[j++];
-
-	delete[] left;
-	delete[] right;
 }
 
 // merges each of the sorted segments of the original data array
-void merge_threads(int data[], merge_sort_thread_args *args, int start, int end){
+void merge_threads(DATA_VECTOR &data, merge_sort_thread_args *args, int start, int end){
 	// start and end refer to segments of the original array that were sorted by each thread
 	if((end - start) == 1){ // base case: merge two segments together
 		merge(data, args[start].start, args[start].end, args[end].end);
@@ -82,17 +78,18 @@ void *merge_sort_thread(void *argp){
 
 		merge_sort_thread((void *) &arg1);
 		merge_sort_thread((void *) &arg2);
-		merge(args->data, args->start, mid, args->end);
+		merge(*args->data, args->start, mid, args->end);
 	}
 
 	return NULL;
 }
 
-int merge_sort(int data[], int length, int max_threads){
+int merge_sort(DATA_VECTOR *data, int max_threads){
 	if(max_threads < 1){
 		return -1;
 	}
 
+	int length = data->size();
 	int num_threads = (max_threads > length) ? length : max_threads;
 
 	pthread_t threads[num_threads];
@@ -127,7 +124,7 @@ int merge_sort(int data[], int length, int max_threads){
 	// merge the sorted segments of the array
 	if(num_threads > 1){
 		// don't need to merge the results from each thread if there's only one thread
-		merge_threads(data, args, 0, num_threads - 1);
+		merge_threads(*data, args, 0, num_threads - 1);
 	}
 
 	return 0;
@@ -137,54 +134,41 @@ int merge_sort(int data[], int length, int max_threads){
 
 #include <time.h>
 
-#define SIZE 10
-#define MAX_NUM 100
-#define NUM_THREADS 2
+#define SIZE 100
+#define MAX_NUM 1000
+#define NUM_THREADS 4
 
-int check_sorted(int *arr, int n){
-	for(int i = 1; i < n; i++){
-		if(arr[i] < arr[i-1]){
+int check_sorted_vector(DATA_VECTOR v){
+	for(int i = 1; i < v.size(); i++){
+		if(v[i].grade < v[i-1].grade){
 			return 0;
 		}
 	}
 	return 1;
 }
 
-void print_arr(int *arr, int n){
-	printf("{");
-	for(int i = 0; i < n; i++){
-		printf("%d ", arr[i]);
+void print_vector(DATA_VECTOR v){
+	printf(">>> START\n");
+	for(int i = 0; i < v.size(); i++){
+		printf("%10d\t%10f\n", v[i].studentID, v[i].grade);
 	}
-	printf("}\n");
+	printf("<<< END\n\n");
 }
 
 int main(){
 	srand((unsigned) time(NULL));
-	int arr[SIZE];
-	for(int i = 0; i < SIZE; i++)
-		arr[i] = rand() % MAX_NUM;
-	print_arr(arr, SIZE);
-	merge_sort(arr, SIZE, NUM_THREADS);
-	print_arr(arr, SIZE);
-	
-	/*
-	int arr[10] = {6, 34, 2, 10, 11, 42, 23, 81, 36, 67};
-	print_arr(arr, 10);
-	merge_sort(arr, 10, 2);
-	print_arr(arr, 10);
-	*/
-	/*
-	int arr[10] = {4, 20, 99, 10, 11, 1, 21, 55, 42, 69};
-	print_arr(arr, 10);
-	merge(arr, 0, 2, 4);
-	print_arr(arr, 10);
-	merge(arr, 5, 7, 9);
-	print_arr(arr, 10);
-	merge(arr, 0, 4, 9);
-	print_arr(arr, 10);
-	*/
+	DATA_VECTOR data;
+	student_data entry;
+	for(int i = 0; i < SIZE; i++){
+		entry.studentID = i;
+		entry.grade = (float) (rand() % MAX_NUM);
+		data.push_back(entry);
+	}
+	print_vector(data);
+	merge_sort(&data, NUM_THREADS);
+	print_vector(data);
 
-	if(check_sorted(arr, SIZE))
+	if(check_sorted_vector(data))
 		printf("SORTED\n");
 	else
 		printf("NOT SORTED\n");
