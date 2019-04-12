41c01f2d46d4f7b7e27919113fbda32dcaace6e9
Aaron Chang <aaronchang153@gmail.com>
2019-04-12 16:52:42 -0700

    Added output functions, changed grade to double, added stats struct

--- csv.h
+++ csv.h
@@ -6,9 +6,19 @@
 
 typedef struct{
     std::string studentID;
-    float grade;
+    double grade;
 } student_data;
 
+typedef struct{
+	double avg;
+	double med;
+	double std_dev;
+} stats_t;
+
 std::vector<student_data> read_csv(std::string fname);
+
+int write_csv(std::string fname, std::vector<student_data> data);
+
+int write_stats_csv(std::string fname, stats_t stats);
 
 #endif // CSV_H
