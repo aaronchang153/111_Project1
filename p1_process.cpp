b5cf4043f27454d02faed03aa50a9b4a8626bb22
Aaron Chang <aaronchang153@gmail.com>
2019-04-12 14:23:23 -0700

    Started implementing processes. Functionality not verified yet

--- p1_process.cpp
+++ p1_process.cpp
@@ -1,16 +1,43 @@
 #include "p1_process.h"
+#include "csv.h"
+#include <sys/wait.h>
+
+#define NUM_FILES 5
 
 
 void get_statistics(std::string class_name[], int num_processes, int num_threads) {
+	if(num_processes > NUM_FILES){
+		num_processes = NUM_FILES;
+	}
+	
+	int process_count = 0; // keeps track of the number of child processes
+	pid_t fork_id;
+	
+	for(int i = 0; i < NUM_FILES; i++){
+		if(process_count < num_processes){
+			fork_id = fork();
+			if(fork_id > 0){ // if this is the parent process
+				printf("Cild process is created. (pid: %d)\n", fork_id);
+				process_count++;
+			}
+			else if(fork_id == 0){ // if this is the child process
+				printf("%d:\tWorking on file: %s", fork_id, class_name[i].c_str());
+				std::vector<student_data> data = read_csv(class_name[i]);
+				merge_sort(&data, num_threads);
+				// output sorted data
+				// calculate and output the data
+			}
+		}
+		else{
+			fork_id = wait(NULL);
+			printf("Child process is terminated. (pid: %d)\n", fork_id);
+			process_count--;
+		}
+	}
 
-	// You need to create multiple processes here (each process
-	// handles at least one file.)
-	
-	
-	// Each process should use the sort function which you have defined  		
-	// in the p1_threads.cpp for multithread sorting of the data. 
-	
-	// Your code should be able to get the statistics and generate
-	// the required outputs for any number of child processes and threads.
-	
+	while(process_count > 0){
+		fork_id = wait(NULL);
+		printf("Child process is terminated. (pid: %d)\n", fork_id);
+		process_count--;
+	}
 }
